936a6be5d9e3409452e6fb87e831013914a7d10d
chaerim <qkrdbsk28@naver.com>
2022-08-30 01:00:18 +0900

    mysql setting

--- swu_backend_01/src/main/java/com/likelion/swu_backend_01/SwuBackend01Application.java
+++ swu_backend_01/src/main/java/com/likelion/swu_backend_01/SwuBackend01Application.java
@@ -2,8 +2,10 @@ package com.likelion.swu_backend_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/env.properties")
 public class SwuBackend01Application {
 
     public static void main(String[] args) {
