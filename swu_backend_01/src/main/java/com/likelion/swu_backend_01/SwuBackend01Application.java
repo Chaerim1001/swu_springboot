825cd78626cb8035e237dc1abe07a04e2df499d3
chaerim <qkrdbsk28@naver.com>
2022-08-30 00:28:42 +0900

    project first settings

---
+++ swu_backend_01/src/main/java/com/likelion/swu_backend_01/SwuBackend01Application.java
@@ -0,0 +1,13 @@
+package com.likelion.swu_backend_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwuBackend01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwuBackend01Application.class, args);
+    }
+
+}
